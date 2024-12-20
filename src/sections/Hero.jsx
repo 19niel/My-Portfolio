bc2d01b58124e14a3de76ae211e51501974c055c
19niel <kungfayla@gmail.com>
2024-12-20 19:51:07 +0800

    tapos na About section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,9 +3,14 @@ import {Canvas} from "@react-three/fiber"
 import { PerspectiveCamera } from '@react-three/drei'
 import HackerRoom from '../components/HackerRoom'
 import CanvasLoader from '../components/CanvasLoader'
-
 import { useMediaQuery } from 'react-responsive'
 import {calculateSizes} from '../constants/index.js'
+import Target from '../components/Target.jsx'
+import ReactLogo from '../components/ReactLogo.jsx'
+import Cube from '../components/Cube.jsx'
+import Rings from '../components/Rings.jsx'
+import HeroCamera from '../components/HeroCamera.jsx'
+import Button from '../components/Button.jsx'
 
 const Hero = () => {
     
@@ -27,21 +32,36 @@ const Hero = () => {
             <Canvas className="w-full h-full">
                 <Suspense fallback={<CanvasLoader/>}>
 
-                <PerspectiveCamera makeDefault position={[0,0,30]}/>
+                <PerspectiveCamera makeDefault position={[0,0,20]}/>
 
-                <HackerRoom
-                 //scale={0.07} 
-                 // position={[0, 0, 0]} 
-                 // rotation={[0, 280, 0]}
-                 position={[2, -8, 2 ]} 
-                 rotation={[0, -Math.PI, 0]}
-                 scale={isMobile ? 0.07 : 0.1}
-                 />
+                <HeroCamera isMobile={isMobile}>                
+                    <HackerRoom
+                    //position={[2, -8, 2 ]}
+                    //scale={isMobile ? 0.07 : 0.1} 
+                    rotation={[0, -Math.PI, 0]}
+                    position={sizes.deskPosition}
+                    scale={sizes.deskScale}
+                    />
+                </HeroCamera>
+
+                 <group>
+                    <Target position={sizes.targetPosition} />
+                    <ReactLogo position={sizes.reactLogoPosition}/>
+                    <Cube position={sizes.cubePosition}/>
+                    <Rings position={sizes.ringPosition}/>
+                 </group>
           
                 <ambientLight intensity={1}/>
                 <directionalLight position={[10,10,10]} intensity={0.5}/>
                 </Suspense>
             </Canvas>
+        </div>
+
+        <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space" >
+
+            <a href="contact" className="w-fit">
+                <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-96"/>
+            </a>
         </div>
     </section>
   )
